a2d034d6c42537b677ead4102d08189b1fda1a51
alialldoboni <alialldoboni@gmail.com>
2026-08-02 22:20:09 +0400

    Refactor App component to implement dynamic step messages and improve step indicator logic

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,13 +1,20 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
 export default function App() {
+  const step = 3;
   return (
     <div className="steps">
       hello react
       <div className="numbers">
-        <div className="active">1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={`step ${step === 1 ? "active" : ""}`}>1</div>
+        <div className={`step ${step === 2 ? "active" : ""}`}>2</div>
+        <div className={`step ${step === 3 ? "active" : ""}`}>3</div>
       </div>
-      <p className="message">hello</p>
+      <p className="message">{messages[step - 1]}</p>
       <div className="buttons">
         <button style={{ backgroundColor: "#790054af", color: "#ffffffff" }}>
           prev
