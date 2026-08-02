4b369847a217d4abd7c2128b73370c59528cd38a
alialldoboni <alialldoboni@gmail.com>
2026-08-02 22:09:45 +0400

    Update App component to enhance step navigation styling and button functionality

--- steps/src/App.js
+++ steps/src/App.js
@@ -3,14 +3,18 @@ export default function App() {
     <div className="steps">
       hello react
       <div className="numbers">
-        <div>1</div>
+        <div className="active">1</div>
         <div>2</div>
         <div>3</div>
       </div>
       <p className="message">hello</p>
       <div className="buttons">
-        <button>prev</button>
-        <button>next</button>
+        <button style={{ backgroundColor: "#790054af", color: "#ffffffff" }}>
+          prev
+        </button>
+        <button style={{ backgroundColor: "#790054af", color: "#ffffffff" }}>
+          next
+        </button>
       </div>
     </div>
   );
