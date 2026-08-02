9a4b2d38ecd35142a01668ce43dad6a01f4a25a7
alialldoboni <alialldoboni@gmail.com>
2026-08-02 22:01:44 +0400

    Refactor App component to remove boilerplate and implement step navigation layout

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="steps">
+      hello react
+      <div className="numbers">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+      <p className="message">hello</p>
+      <div className="buttons">
+        <button>prev</button>
+        <button>next</button>
+      </div>
     </div>
   );
 }
-
-export default App;
